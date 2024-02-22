a12049ed806a985c85cad38597a849f84d019435
Matheus Roika <matheusroika@hotmail.com>
2024-02-22 05:01:41 -0300

    feat: add a blob size check and alert in case nothing was recorded

--- src/components/RecordingButtons.tsx
+++ src/components/RecordingButtons.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { Button } from '@/components/shadcn/ui/button'
+import { Alert, AlertDescription, AlertTitle } from "@/components/shadcn/ui/alert"
 import { useApp } from '@/contexts/AppProvider'
 import { Buffer } from 'buffer'
 import fixWebmDuration from 'fix-webm-duration'
+import { AlertCircle } from 'lucide-react'
 const api = window.api
 
 export function RecordingButtons() {
@@ -11,6 +13,8 @@ export function RecordingButtons() {
   const [startTime, setStartTime] = useState(0)
   const [pauseStartTime, setPauseStartTime] = useState(0)
   const [pauseDuration, setPauseDuration] = useState(0)
+  const [alert, setAlert] = useState({ title: '', description: '' })
+  const [showAlert, setShowAlert] = useState(false)
   const { mediaRecorder } = useApp()
   let recordedChunks = [] as Blob[]
 
@@ -26,6 +30,19 @@ export function RecordingButtons() {
     setStartTime(0)
     setPauseStartTime(0)
     setPauseDuration(0)
+    if (blob.size === 0) {
+      setAlert({ title: 'Video not saved', description: 'No data received - Try clicking and holding to record' })
+      setTimeout(() => {
+        setShowAlert(true)
+      }, 100)
+      setTimeout(() => {
+        setShowAlert(false)
+        setTimeout(() => {
+          setAlert({ title: '', description: '' })
+        }, 150)
+      }, 3000)
+      return
+    }
     const fixedBlob = await fixWebmDuration(blob, videoDuration)
     const arrayBuffer = await fixedBlob.arrayBuffer()
     const buffer = Buffer.from(arrayBuffer)
@@ -76,11 +93,23 @@ export function RecordingButtons() {
   })
 
   return (
+    <>
     <div className='flex flex-col gap-4'>
       <Button disabled={isRecording} onClick={startRecording}>Start Recording</Button>
       <Button disabled={!isRecording} onClick={stopRecording}>Stop Recording</Button>
-      <button id='resumeRecording' className='' onClick={resumeRecording} disabled={!isPaused}>Resume</button>
-      <button id='pauseRecording' className='' onClick={pauseRecording} disabled={isPaused}>Pause</button>
+      <button id='resumeRecording' className='hidden' onClick={resumeRecording} disabled={!isPaused}>Resume</button>
+      <button id='pauseRecording' className='hidden' onClick={pauseRecording} disabled={isPaused}>Pause</button>
     </div>
+
+    <Alert className='fixed top-6 left-6 w-fit transition' style={{ opacity: showAlert ? '1' : '0', display: alert.title ? 'block' : 'none' }}>
+      <div className='flex justify-center items-center gap-4'>
+        <AlertCircle className='h-4 w-4' />
+        <div>
+          <AlertTitle>{alert.title}</AlertTitle>
+          <AlertDescription>{alert.description}</AlertDescription>
+        </div>
+      </div>
+    </Alert>
+    </>
   )
 }
